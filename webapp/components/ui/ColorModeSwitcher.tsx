b5572082929723a3b63763f83968b4741474e2f8
Ludal <ludovic.chombeau@gmail.com>
2021-12-02 19:39:06 +0100

    feat(ColorModeSwitcher): add toggle sound

--- webapp/components/ui/ColorModeSwitcher.tsx
+++ webapp/components/ui/ColorModeSwitcher.tsx
@@ -1,17 +1,31 @@
 import { IconButton } from "@chakra-ui/button";
 import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import useSound from "use-sound";
+
+const soundId = {
+  dark: "on",
+  light: "off",
+};
 
 const ColorModeSwitcher = () => {
-  const { toggleColorMode } = useColorMode();
+  const { toggleColorMode, colorMode } = useColorMode();
   const icon = useColorModeValue(<MoonIcon />, <SunIcon />);
+  const [play] = useSound("/assets/audio/switch.mp3", {
+    volume: 0.1,
+    sprite: {
+      on: [0, 300],
+      off: [500, 300],
+    },
+  });
+
+  const toggle = () => {
+    toggleColorMode();
+    play({ id: soundId[colorMode] });
+  };
 
   return (
-    <IconButton
-      aria-label="Changer le thème"
-      icon={icon}
-      onClick={toggleColorMode}
-    />
+    <IconButton aria-label="Changer le thème" icon={icon} onClick={toggle} />
   );
 };
 
